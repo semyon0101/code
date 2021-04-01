349a916ff057025c453e5c3537bab6e27b6b0f74
kirsemen <79764144+kirsemen@users.noreply.github.com>
2021-04-01 16:42:39 +0300

    Add files via upload

---
+++ code-messager/мессанджер/test/main.py
@@ -0,0 +1,9 @@
+import connected_user.user as user
+# print(user.user())
+# print(user.db.get_text())
+import getpassvord
+import message_file
+
+# if user.db.get_text() == [("", "", "")]:
+#    if not getpassvord.start():
+message_file.start()
