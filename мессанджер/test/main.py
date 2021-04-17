cb4a70a1e18cfbb2111abfec80a3f20fc58be9aa
kirsemen <79764144+kirsemen@users.noreply.github.com>
2021-04-17 13:59:55 +0300

    Delete мессанджер directory

--- мессанджер/test/main.py
+++
@@ -1,9 +0,0 @@
-import connected_user.user as user
-# print(user.user())
-# print(user.db.get_text())
-import get_password
-import message_file
-
-# if user.db.get_text() == [("", "", "")]:
-#    if not get_password.start():
-message_file.start()
