57578f51b7ed3ac42caf3920c08f2e9f1735fcbf
kirsemen <79764144+kirsemen@users.noreply.github.com>
2021-11-04 22:33:44 +0300

    Update main.py

--- MC/main.py
+++ MC/main.py
@@ -4,19 +4,22 @@ import sys
 import numpy as np
 import numba
 
-size = np.array([1500, 1000])
+size = np.array([1000, 500])
 
 clock = pygame.time.Clock()
 
 speed = 0.5
 
+fast_mode = False
+
 
 def get_object_from_file(filename):
-    vertex, faces = [], []
+    _vertex, _faces = [], []
     with open(filename) as f:
+        m = 0
         for line in f:
             if line.startswith('v '):
-                vertex.append(
+                _vertex.append(
                     list(
                         rotate(0, -90, np.array([float(i) for i in line.split()[1:]]), np.array([0, 0, 0])
                                )
@@ -24,100 +27,125 @@ def get_object_from_file(filename):
                 )
             elif line.startswith('f'):
                 faces_ = line.split()[1:]
-                faces.append([int(face_.split('/')[0]) - 1 for face_ in faces_])
-    return np.array(vertex), faces
+                _arr = [int(face_.split('/')[0]) - 1 for face_ in faces_]
+                _faces.append(_arr)
+                if m < len(_arr):
+                    m = len(_arr)
+    faces_2 = np.zeros([len(_faces), m])
+    for i in range(len(_faces)):
+        face = _faces[i]
+        for j in range(m):
+            if len(face) > j:
+                faces_2[i][j] = face[j]
+            else:
+                faces_2[i][j] = -1
+    return np.array(_vertex), faces_2
 
 
-def normalise(_arr, size):
+@numba.njit(fastmath=True)
+def normalise(_arr, _size):
     new_arr = []
     a = True
     for i in range(len(_arr)):
-        if not _arr[i][2]:
+        if not bool(_arr[i][2]):
             a = False
     if a:
-        return None
+        return [[0.0, 0.0]], False
 
     for i in range(len(_arr)):
         x, y, b = _arr[i]
-        if not b:
+        if not bool(b):
             new_arr.append([x, y])
         else:
             if not _arr[(i - 1) % len(_arr)][2]:
                 x1, y1 = _arr[(i - 1) % len(_arr)][:2]
-                x2, y2 = (x1 - size[0] / 2 + x - size[0] / 2) / 2, (y1 - size[1] / 2 + y - size[1] / 2) / 2
-                new_arr.append([x2 * 1000 + size[0] / 2, y2 * 1000 + size[1] / 2])
+                x2, y2 = (x1 - _size[0] / 2 + x - _size[0] / 2) / 2, (y1 - _size[1] / 2 + y - _size[1] / 2) / 2
+                new_arr.append([x2 * 1000 + _size[0] / 2, y2 * 1000 + _size[1] / 2])
             if not _arr[(i + 1) % len(_arr)][2]:
                 x1, y1 = _arr[(i + 1) % len(_arr)][:2]
-                x2, y2 = (x1 - size[0] / 2 + x - size[0] / 2) / 2, (y1 - size[1] / 2 + y - size[1] / 2) / 2
-                new_arr.append([x2 * 1000 + size[0] / 2, y2 * 1000 + size[1] / 2])
+                x2, y2 = (x1 - _size[0] / 2 + x - _size[0] / 2) / 2, (y1 - _size[1] / 2 + y - _size[1] / 2) / 2
+                new_arr.append([x2 * 1000 + _size[0] / 2, y2 * 1000 + _size[1] / 2])
 
-    return new_arr
-
-
-@numba.njit(fastmath=True)
-def matmul(A, B):
-    C = np.array([0.0, 0.0, 0.0])
-    for i in range(3):
-        for j in range(3):
-            C[i] += B[j][i] * A[j]
-    return C
+    return new_arr, True
 
 
 @numba.njit(fastmath=True)
-def get_m(x: float, y: float):
-    x = math.radians(x)
-    y = math.radians(y)
-    return np.array([
-        [
-            math.cos(x),
-            math.sin(x) * math.cos(y),
-            math.sin(x) * math.sin(y)
-        ],
-        [
-            -math.sin(x),
-            math.cos(x) * math.cos(y),
-            math.cos(x) * math.sin(y)
-        ],
-        [
-            0,
-            -math.sin(y),
-            math.cos(y)
-        ]
-    ])
-
-
-@numba.njit(fastmath=True)
-def rotate(x, y,
-           pos: np.array([float, float, float]),
+def rotate(_x_angle, _y_angle,
+           _pos: np.array([float, float, float]),
            center: np.array([float, float, float])) -> np.array([float, float, float]):
-    pos -= center
-    pos = matmul(pos, get_m(x, y))
-    pos += center
-    return pos
+    _x_angle = math.radians(_x_angle)
+    _y_angle = math.radians(_y_angle)
+
+    _pos -= center
+
+    r_x_y = math.sqrt(_pos[1] ** 2 + _pos[0] ** 2)
+    _x_angle += math.atan2(_pos[1], _pos[0])
+    _pos[0] = r_x_y * math.cos(_x_angle)
+    _pos[1] = r_x_y * math.sin(_x_angle)
+
+    _y_angle += math.atan2(_pos[2], _pos[1])
+    r_y_z = math.sqrt(_pos[1] ** 2 + _pos[2] ** 2)
+    _pos[1] = r_y_z * math.cos(_y_angle)
+    _pos[2] = r_y_z * math.sin(_y_angle)
+
+    _pos += center
+    return _pos
 
 
 @numba.njit(fastmath=True)
-def get_pos_in_2d_plane(pos_player: np.array([float, float, float]),
-                        pos_object: np.array([float, float, float]),
-                        x_angle, y_angle, size):
-    pos_object = rotate(x_angle, y_angle, pos_object, pos_player)
-    if (pos_object[1] - pos_player[1]) == 0:
-        return (pos_object[0] - pos_player[0]) * 10000 + size[0] / 2, \
-               (pos_object[2] - pos_player[2]) * 10000 + size[1] / 2, False
+def get_pos_in_2d_plane(pos_player: np.array([float, float, float]), _x_angle, _y_angle,
+                        _size, _vertex, _faces, _fast_mode):
+    _x_angle = math.radians(_x_angle)
+    _y_angle = math.radians(_y_angle)
+    _arr2D = []
+    for pos_object in _vertex:
+        _x_angle2 = _x_angle
+        _y_angle2 = _y_angle
+        pos_object -= pos_player
 
-    xa = (pos_object[0] - pos_player[0]) / (max(abs(pos_object[1] - pos_player[1]), 0.01) / 4)
-    ya = (pos_object[2] - pos_player[2]) / (max(abs(pos_object[1] - pos_player[1]), 0.01) / 4)
+        r_x_y = math.sqrt(pos_object[1] ** 2 + pos_object[0] ** 2)
+        _x_angle2 += math.atan2(pos_object[1], pos_object[0])
+        pos_object[0] = r_x_y * math.cos(_x_angle2)
+        pos_object[1] = r_x_y * math.sin(_x_angle2)
 
-    x = size[0] / 2 + xa * 100
-    y = size[1] / 2 + ya * 100
-    return x, y, pos_object[1] - pos_player[1] < 0
+        _y_angle2 += math.atan2(pos_object[2], pos_object[1])
+        r_y_z = math.sqrt(pos_object[1] ** 2 + pos_object[2] ** 2)
+        pos_object[1] = r_y_z * math.cos(_y_angle2)
+        pos_object[2] = r_y_z * math.sin(_y_angle2)
 
+        if (pos_object[1]) == 0:
+            _arr2D.append([(pos_object[0]) * 10000 + _size[0] / 2,
+                          (pos_object[2]) * 10000 + _size[1] / 2, False])
 
-pygame.init()
-screen = pygame.display.set_mode(size)
+        xa = (pos_object[0]) / (max(abs(pos_object[1]), 0.01) / 4)
+        ya = (pos_object[2]) / (max(abs(pos_object[1]), 0.01) / 4)
+
+        _arr2D.append([_size[0] / 2 + xa * 100,
+                      _size[1] / 2 + ya * 100,
+                      pos_object[1] < 0])
+
+    _arr = []
+    for face in _faces:
+        buffer = []
+        for i in face:
+            if i != -1:
+                buffer.append(_arr2D[int(i)])
+            else:
+                break
+        arr2, b = normalise(buffer, _size)
+        if _fast_mode:
+            if b and [0 <= x <= _size[0] and 0 <= y <= _size[1] for x, y in arr2].count(True):
+                _arr.append(arr2)
+        else:
+            if b:
+                _arr.append(arr2)
+
+    return _arr
+
 
 vertex, faces = get_object_from_file('resources/t_34_obj.obj')
 pos_pl = np.array([0.5, 0.5, -0.35], np.float_)
+
 # vertex = [
 #     [1, -1, -1],
 #     [1, 1, -1],
@@ -177,7 +205,11 @@ pos_pl = np.array([0.5, 0.5, -0.35], np.float_)
 #     [2, 3],
 #     [3, 0]
 # ]
-x_angle, y_angle = -90, -30
+x_angle, y_angle = -90.1, -30.1
+
+pygame.init()
+screen = pygame.display.set_mode(size)
+
 now_pos = [None, None]
 while True:
     for event in pygame.event.get():
@@ -187,8 +219,7 @@ while True:
 
     keys = pygame.key.get_pressed()
 
-    y_angle = max(-90, min(90, y_angle))
-    x_angle = x_angle % 360
+
     if keys[pygame.K_a]:
         left = rotate(
             -x_angle, 0,
@@ -241,19 +272,14 @@ while True:
             now_pos = pygame.mouse.get_pos()
     else:
         now_pos = [None, None]
+    
+    y_angle = max(-90, min(90, y_angle))
+    x_angle = x_angle % 360
 
-    arr2D = []
-    for pos_obj in vertex:
-        arr2D.append(
-            get_pos_in_2d_plane(pos_pl,
-                                pos_obj.copy(),
-                                x_angle, y_angle, size)
-        )
+    arr = get_pos_in_2d_plane(pos_pl, x_angle, y_angle, size, vertex.copy(), faces, fast_mode)
 
-    for face in faces:
-        answer = normalise([arr2D[i] for i in face], size)
-        if answer:
-            pygame.draw.polygon(screen, [255, 255, 255], answer, 1)
+    for pos in arr:
+        pygame.draw.polygon(screen, [255, 255, 255], pos, 1)
 
     pygame.display.update()
     screen.fill((0, 0, 0))
